8604bee8f18f3a6a9bded58c1bbb162dc2996e74
Joshua <josh.reid@scoutspace.io>
2026-06-22 07:20:21 -0500

    Fix homepage hash reveal behavior

--- app/javascript/controllers/reveal_controller.js
+++ app/javascript/controllers/reveal_controller.js
@@ -8,11 +8,19 @@ export default class extends Controller {
   }
 
   connect() {
+    this.handleHashChange = this.handleHashChange.bind(this)
     this.element.classList.add("reveal--pending")
     this.element.style.transitionDelay = `${this.delayValue || 0}ms`
 
     if (this.prefersReducedMotion || !("IntersectionObserver" in window)) {
       this.reveal()
+      return
+    }
+
+    window.addEventListener("hashchange", this.handleHashChange)
+
+    if (this.isInsideHashTarget()) {
+      this.revealImmediately()
       return
     }
 
@@ -27,19 +35,52 @@ export default class extends Controller {
   }
 
   disconnect() {
+    window.removeEventListener("hashchange", this.handleHashChange)
     this.observer?.disconnect()
     this.element.style.transitionDelay = ""
+    this.element.style.transition = ""
   }
 
   handleIntersection(entries) {
     if (entries.some((entry) => entry.isIntersecting)) {
-      this.reveal()
+      if (this.isInsideHashTarget()) {
+        this.revealImmediately()
+      } else {
+        this.reveal()
+      }
+    }
+  }
+
+  handleHashChange() {
+    if (this.isInsideHashTarget()) {
+      this.revealImmediately()
     }
   }
 
   reveal() {
     this.element.classList.add("reveal--visible")
     this.observer?.unobserve(this.element)
+  }
+
+  revealImmediately() {
+    this.element.style.transition = "none"
+    this.element.style.transitionDelay = "0ms"
+    this.reveal()
+
+    requestAnimationFrame(() => {
+      this.element.style.transition = ""
+    })
+  }
+
+  isInsideHashTarget() {
+    if (!window.location.hash) return false
+
+    try {
+      const target = document.querySelector(window.location.hash)
+      return Boolean(target?.contains(this.element))
+    } catch {
+      return false
+    }
   }
 
   get prefersReducedMotion() {
