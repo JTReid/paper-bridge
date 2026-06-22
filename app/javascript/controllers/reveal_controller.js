b441e284162f6d4944f8a437f8f2e644aa5dfc25
Joshua <josh.reid@scoutspace.io>
2026-06-22 07:29:26 -0500

    Resolve adjacent homepage reveal on hash navigation

--- app/javascript/controllers/reveal_controller.js
+++ app/javascript/controllers/reveal_controller.js
@@ -1,13 +1,37 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static hashNavigationSettlesAt = 0
+  static hashNavigationClickListener = null
+
   static values = {
     delay: Number,
     rootMargin: { type: String, default: "0px 0px -8% 0px" },
     threshold: { type: Number, default: 0.12 }
   }
 
+  static ensureHashNavigationClickListener() {
+    if (this.hashNavigationClickListener) return
+
+    this.hashNavigationClickListener = (event) => {
+      const link = event.target.closest("a[href]")
+      if (!link) return
+
+      const url = new URL(link.href, window.location.href)
+      if (url.origin !== window.location.origin || url.pathname !== window.location.pathname || !url.hash) return
+
+      this.markHashNavigationSettling()
+    }
+
+    document.addEventListener("click", this.hashNavigationClickListener, true)
+  }
+
+  static markHashNavigationSettling() {
+    this.hashNavigationSettlesAt = performance.now() + 700
+  }
+
   connect() {
+    this.constructor.ensureHashNavigationClickListener()
     this.handleHashChange = this.handleHashChange.bind(this)
     this.element.classList.add("reveal--pending")
     this.element.style.transitionDelay = `${this.delayValue || 0}ms`
@@ -36,6 +60,7 @@ export default class extends Controller {
 
   disconnect() {
     window.removeEventListener("hashchange", this.handleHashChange)
+    cancelAnimationFrame(this.hashNavigationFrame)
     this.observer?.disconnect()
     this.element.style.transitionDelay = ""
     this.element.style.transition = ""
@@ -43,7 +68,7 @@ export default class extends Controller {
 
   handleIntersection(entries) {
     if (entries.some((entry) => entry.isIntersecting)) {
-      if (this.isInsideHashTarget()) {
+      if (this.isInsideHashTarget() || this.isHashNavigationSettling()) {
         this.revealImmediately()
       } else {
         this.reveal()
@@ -52,9 +77,18 @@ export default class extends Controller {
   }
 
   handleHashChange() {
+    this.constructor.markHashNavigationSettling()
+
     if (this.isInsideHashTarget()) {
       this.revealImmediately()
+      return
     }
+
+    this.hashNavigationFrame = requestAnimationFrame(() => {
+      if (this.isInViewport()) {
+        this.revealImmediately()
+      }
+    })
   }
 
   reveal() {
@@ -65,10 +99,14 @@ export default class extends Controller {
   revealImmediately() {
     this.element.style.transition = "none"
     this.element.style.transitionDelay = "0ms"
+    this.element.getBoundingClientRect()
     this.reveal()
+    this.element.getBoundingClientRect()
 
     requestAnimationFrame(() => {
-      this.element.style.transition = ""
+      requestAnimationFrame(() => {
+        this.element.style.transition = ""
+      })
     })
   }
 
@@ -81,6 +119,15 @@ export default class extends Controller {
     } catch {
       return false
     }
+  }
+
+  isHashNavigationSettling() {
+    return performance.now() <= this.constructor.hashNavigationSettlesAt
+  }
+
+  isInViewport() {
+    const rect = this.element.getBoundingClientRect()
+    return rect.top < window.innerHeight && rect.bottom > 0
   }
 
   get prefersReducedMotion() {
